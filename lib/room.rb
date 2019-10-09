a72023b9ea3df66b1412364c5ee0a43859e9dc69
Ana Perez Santos <a.jperez@yahoo.com>
2019-10-09 09:17:26 -0600

    Add setup, first test, assert instance, and create room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,4 +1,6 @@
+class Room
 
+end
 
 
 # Iteration 1
--- test/room_test.rb
+++ test/room_test.rb
@@ -1,1 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
 
+class RoomTest < Minitest::Test
+  def setup
+    room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_it_exists
+    assert_instance_of Room, room
+  end
+end
